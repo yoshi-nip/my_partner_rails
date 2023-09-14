ac44de3f1b0613c97521f9413f5f835d7e5e7db9
yoshi <mdmdmdmdmd57@gmail.com>
2023-09-14 17:00:20 +0900

    GithubActionsが動作し弾かれたところを修正、bundle exec rubocop -Aの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #  index_users_on_uid_and_provider      (uid,provider) UNIQUE
 #
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
