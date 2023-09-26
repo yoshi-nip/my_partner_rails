8b593e493c6ca0dc609af2a9f4524a927d2f08c0
yoshi <mdmdmdmdmd57@gmail.com>
2023-09-26 15:17:12 +0900

    day_articleモデルのテストを記述

--- spec/factories/day_articles.rb
+++ spec/factories/day_articles.rb
@@ -1,4 +1,12 @@
+# FactoryBot.define do
+#   factory :day_article do
+#     day {  }
+#     body {  }
+#   end
+# end
 FactoryBot.define do
   factory :day_article do
+    day { Faker::Date.between(from: "2020-01-01", to: "2022-12-31") }
+    body { Faker::Lorem.paragraphs(number: 3).join(" ") }
   end
 end
