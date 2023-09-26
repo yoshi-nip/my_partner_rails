e04a388d398bc188a52c1e299c02446b1ea6c5ca
yoshi <mdmdmdmdmd57@gmail.com>
2023-09-26 15:20:41 +0900

    rubocopを行い、修正

--- spec/models/day_article_spec.rb
+++ spec/models/day_article_spec.rb
@@ -3,34 +3,33 @@ require "rails_helper"
 RSpec.describe DayArticle, type: :model do
   context "正しく情報が指定されているとき" do
     let!(:user) { create(:user) }
-    let!(:day_article) { build(:day_article,user_id: user.id) }
-    fit "ユーザーが作成される" do
+    let!(:day_article) { build(:day_article, user_id: user.id) }
+    it "ユーザーが作成される" do
       expect(day_article).to be_valid
     end
 
-    fit 'ユーザーとの関連付けが定義されている' do
-      association = described_class.reflect_on_association(:user)
+    it "ユーザーとの関連付けが定義されている" do
+      association = DayArticle.reflect_on_association(:user)
       expect(association.macro).to eq(:belongs_to)
     end
   end
 
-  #異常系
+  # 異常系
   context "正しく情報が指定されていない時" do
     let!(:user) { create(:user) }
     let(:day_article_user_blank) { build(:day_article) }
-    let(:day_article_body_blank) { build(:day_article,body: "") }
-    let(:day_article_day_blank) { build(:day_article,day:"") }
-    fit "ユーザーがない時記事が作成されない" do
+    let(:day_article_body_blank) { build(:day_article, body: "", user_id: user.id) }
+    let(:day_article_day_blank) { build(:day_article, day: "", user_id: user.id) }
+    it "ユーザーがない時記事が作成されない" do
       expect(day_article_user_blank).not_to be_valid
     end
 
-    fit "日付がない時記事が作成されない" do
-      expect(day_article_user_blank).not_to be_valid
+    it "日付がない時記事が作成されない" do
+      expect(day_article_day_blank).not_to be_valid
     end
 
-    fit "本文がない時記事が作成されない" do
-      expect(day_article_user_blank).not_to be_valid
+    it "本文がない時記事が作成されない" do
+      expect(day_article_body_blank).not_to be_valid
     end
-
   end
 end
