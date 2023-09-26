54f0b42f92b4b912383e6e143511cc9f7bce09ad
yoshi <mdmdmdmdmd57@gmail.com>
2023-09-26 17:01:59 +0900

    weekly機能の削除

--- spec/factories/day_articles.rb
+++ spec/factories/day_articles.rb
@@ -1,9 +1,23 @@
-# FactoryBot.define do
-#   factory :day_article do
-#     day {  }
-#     body {  }
-#   end
-# end
+# == Schema Information
+#
+# Table name: day_articles
+#
+#  id         :bigint           not null, primary key
+#  body       :string
+#  day        :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_day_articles_on_day      (day) UNIQUE
+#  index_day_articles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :day_article do
     day { Faker::Date.between(from: "2020-01-01", to: "2022-12-31") }
--- spec/factories/habits.rb
+++ spec/factories/habits.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: habits
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  start_date :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_habits_on_start_date  (start_date) UNIQUE
+#  index_habits_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :habit do
   end
