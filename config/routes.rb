1b8c258835fe4655394ff5406d6a51564e82ab04
yoshi <mdmdmdmdmd57@gmail.com>
2023-09-29 08:42:56 +0900

    divise-token-authの新規登録機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
     namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth", controllers: {
+        registrations: "api/v1/auth/registrations",
+      }
       resources :day_articles, only: [:index, :create, :show, :update, :destroy]
       resources :monthly_articles, only: [:index, :create, :show, :update, :destroy]
       resources :monthly_promises, only: [:index, :create, :show, :update, :destroy]
