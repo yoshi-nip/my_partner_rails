fa1485b3c2c0dc3a38951b0310e72d54994b47b0
yoshi <mdmdmdmdmd57@gmail.com>
2023-09-29 08:57:27 +0900

    divise-token-authの設定の修正、テストの作成

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
+
     def sign_up_params
       params.require(:registration).permit(:name, :email, :password, :password_confirmation)
     end
--- spec/requests/api/v1/auth/registrations_spec.rb
+++ spec/requests/api/v1/auth/registrations_spec.rb
@@ -1,7 +1,27 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Api::V1::Auth::Registrations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /api/v1/auth" do
+    subject { post(api_v1_user_registration_path, params: user_params) }
+
+    context "適切なパラメータが送信されたとき" do
+      let(:user_params) { { registration: attributes_for(:user) } }
+      it "新規ユーザーが作られる" do
+        expect { subject }.to change { User.count }.by(1)
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq "success"
+        expect(res["data"]["name"]).to eq user_params[:registration][:name]
+        expect(res["data"]["email"]).to eq user_params[:registration][:email]
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "想定したヘッダー情報が返ってくる" do
+        subject
+        expected_headers = ["token-type", "access-token", "client", "uid", "expiry", "authorization"]
+        expected_headers.each do |header_key|
+          expect(response.header[header_key]).to be_present
+        end
+      end
+    end
   end
 end
