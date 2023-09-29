06d1af218a8cb99cf2b47a27874bcfd7a6796692
yoshi <mdmdmdmdmd57@gmail.com>
2023-09-29 09:08:45 +0900

    新規登録機能、異常系テストも作成

--- spec/requests/api/v1/auth/registrations_spec.rb
+++ spec/requests/api/v1/auth/registrations_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Api::V1::Auth::Registrations", type: :request do
   describe "POST /api/v1/auth" do
     subject { post(api_v1_user_registration_path, params: user_params) }
-
+    #正常系
     context "適切なパラメータが送信されたとき" do
       let(:user_params) { { registration: attributes_for(:user) } }
       it "新規ユーザーが作られる" do
@@ -15,12 +15,44 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
         expect(response).to have_http_status(:ok)
       end
 
-      it "想定したヘッダー情報が返ってくる" do
+      it "想定したtokenのヘッダー情報が返ってくる" do
         subject
         expected_headers = ["token-type", "access-token", "client", "uid", "expiry", "authorization"]
         expected_headers.each do |header_key|
           expect(response.header[header_key]).to be_present
         end
+      end
+    end
+
+    #異常系
+    context "不適切なパラメータが送れた時" do
+      let(:user_params) { attributes_for(:user) }
+      it "エラーが返ってくる" do
+        expect { subject }.to raise_error ActionController::ParameterMissing
+      end
+    end
+
+    context "nameがない時" do
+      let(:user_params) { { registration: attributes_for(:user, name: nil) } }
+      it "エラーが返ってくる" do
+        subject
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "emailがない時" do
+      let(:user_params) { { registration: attributes_for(:user, email: nil) } }
+      it "エラーが返ってくる" do
+        subject
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "passwordがない時" do
+      let(:user_params) { { registration: attributes_for(:user, password: nil) } }
+      it "エラーが返ってくる" do
+        subject
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
